f2c6f775939b2dcbe4d5e3bd8231a5fb836c8fb8
snap505 <lorenzobauwens@icloud.com>
2026-05-19 20:59:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const colors = ["green", "red", "blue","yellow","purple","pink","cyan"];
+const colors = ["green", "red", "blue", "yellow", "purple", "pink", "cyan"];
 
 document.querySelectorAll(".color-btn").forEach(btn => {
     let index = 0;
 
     btn.addEventListener("click", () => {
-    index = (index + 1) % colors.length;
-    btn.style.background = colors[index];
+        index = (index + 1) % colors.length;
+        btn.style.background = colors[index];
     });
 });
